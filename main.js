c16a546256744631530ceba8c2bb34980651db3c
chows23 <tsrn34388644@gmail.com>
2020-12-01 20:01:45 +0800

    adds addEventListener to the script to change footer color.

--- main.js
+++ main.js
@@ -2,8 +2,13 @@
 const color = document.getElementById('colorPicker');
 
 const navColor = document.querySelector('.navColor');
+const footerColor = document.querySelector('.footerColor');
 
 navColor.addEventListener('click', function(event) {
     event.target.style.backgroundColor = color.value;
+});
+
+footerColor.addEventListener('click', function(event) {
+  event.target.style.backgroundColor = color.value;
 });
 
