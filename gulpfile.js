5eea1cfa0461c05c40273e5f301414007e8448a3
chows23 <tsrn34388644@gmail.com>
2020-12-07 14:35:25 +0800

    minifyand sourcemap css and js files.

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,30 @@ const {src, dest, series, parallel} = require('gulp');
 const concat = require('gulp-concat');
 const imagemin = require('gulp-imagemin');
 
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify-es').default;
+const cssnano = require('gulp-cssnano');
+
 function htmlTask() {
   return src('src/*.html')
     .pipe(dest('dist'))
-}
+};
 
 function styleTask() {
   return src('src/styles/*.css')
     .pipe(concat('all.css'))
+    .pipe(sourcemaps.init())
+    .pipe(cssnano())
+    .pipe(sourcemaps.write())
     .pipe(dest('dist/css'))
-}
+};
 
 function scriptTask() {
   return src('src/scripts/*.js')
+    .pipe(concat('main.js'))
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(dest('dist/js'))
 }
 
